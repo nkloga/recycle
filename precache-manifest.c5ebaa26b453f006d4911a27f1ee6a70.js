b09d7c3652ab67052e4f754bde3433b23951ea06
nkloga <n.kloga@gmail.com>
2019-11-30 20:51:22 +0200

    Updates

---
+++ precache-manifest.c5ebaa26b453f006d4911a27f1ee6a70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "698cff3a734ecace1290754b5eaa646b",
+    "url": "/recycle/index.html"
+  },
+  {
+    "revision": "2f522d9bddc1eecad44d",
+    "url": "/recycle/static/css/main.7a000800.chunk.css"
+  },
+  {
+    "revision": "d8ee31350ff8e4e95076",
+    "url": "/recycle/static/js/2.3ff52473.chunk.js"
+  },
+  {
+    "revision": "2f522d9bddc1eecad44d",
+    "url": "/recycle/static/js/main.ffded825.chunk.js"
+  },
+  {
+    "revision": "37f867ee7bbeb3539149",
+    "url": "/recycle/static/js/runtime-main.6bc93aec.js"
+  }
+]);
